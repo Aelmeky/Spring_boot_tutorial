c7614e74c6f7331be92900d8425b439f26eecb98
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-07-09 09:34:50 +0300

    adding path variable

--- tutorial/src/main/java/springboot/tutorial/StudetController.java
+++ tutorial/src/main/java/springboot/tutorial/StudetController.java
@@ -1,14 +1,20 @@
 package springboot.tutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudetController {
-	
+
 	@GetMapping("/student")
 	public student getStudent() {
 		return new student("hima", "ahmed");
+	}
+
+	@GetMapping("/student/{firstName}/{lastName}")
+	public student studentPathVariable(@PathVariable String firstName, String lastName) {
+		return new student(firstName, lastName);
 	}
 
 }
