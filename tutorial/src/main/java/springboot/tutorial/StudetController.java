d240381eee3e17f3bcc0efccbdf81217d0ac6e32
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-07-09 10:01:43 +0300

    adding query variable

--- tutorial/src/main/java/springboot/tutorial/StudetController.java
+++ tutorial/src/main/java/springboot/tutorial/StudetController.java
@@ -2,6 +2,7 @@ package springboot.tutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,15 @@ public class StudetController {
 	public student getStudent() {
 		return new student("hima", "ahmed");
 	}
-
+// path variables
 	@GetMapping("/student/{firstName}/{lastName}")
-	public student studentPathVariable(@PathVariable String firstName, String lastName) {
+	public student studentPathVariable(@PathVariable String firstName,@PathVariable String lastName) {
+		return new student(firstName, lastName);
+	}
+	
+	// query varibles 
+	@GetMapping("/student/query")
+	public student studentQueryVariable(@RequestParam String firstName,@RequestParam String lastName) {
 		return new student(firstName, lastName);
 	}
 
